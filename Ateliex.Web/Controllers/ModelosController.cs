b7c253b594bb9938ec88f46a88920c20cf0a28eb
Marcelo Dias <mardsystems@gmail.com>
2020-04-06 15:12:17 -0300

    Adição do controller de modelos.

---
+++ Ateliex.Web/Controllers/ModelosController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Ateliex.Data;
+using Ateliex.Models;
+
+namespace Ateliex.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelosController : ControllerBase
+    {
+        private readonly AteliexDbContext db;
+
+        public ModelosController(AteliexDbContext db)
+        {
+            this.db = db;
+        }
+
+        // GET: api/Modelos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Modelo>>> GetModelos()
+        {
+            return await db.Modelos.ToListAsync();
+        }
+
+        // GET: api/Modelos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Modelo>> GetModelo(string id)
+        {
+            var modelo = await db.Modelos.FindAsync(id);
+
+            if (modelo == null)
+            {
+                return NotFound();
+            }
+
+            return modelo;
+        }
+
+        // PUT: api/Modelos/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutModelo(string id, Modelo modelo)
+        {
+            if (id != modelo.Codigo)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(modelo).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ModeloExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Modelos
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Modelo>> PostModelo(Modelo modelo)
+        {
+            db.Modelos.Add(modelo);
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ModeloExists(modelo.Codigo))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetModelo", new { id = modelo.Codigo }, modelo);
+        }
+
+        // DELETE: api/Modelos/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Modelo>> DeleteModelo(string id)
+        {
+            var modelo = await db.Modelos.FindAsync(id);
+            if (modelo == null)
+            {
+                return NotFound();
+            }
+
+            db.Modelos.Remove(modelo);
+            await db.SaveChangesAsync();
+
+            return modelo;
+        }
+
+        private bool ModeloExists(string id)
+        {
+            return db.Modelos.Any(e => e.Codigo == id);
+        }
+    }
+}
