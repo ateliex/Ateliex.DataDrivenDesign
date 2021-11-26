f22572afe1c191afa7925625ee5f3b187746b04c
Marcelo Dias <mardsystems@gmail.com>
2021-11-25 22:22:38 -0300

    task: recria esquema de identidade

--- src/Ateliex.Web/Data/ApplicationDbContext.cs
+++ src/Ateliex.Web/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ateliex.Data;
@@ -9,5 +10,31 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
 
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<IdentityRole>()
+            .ToTable("Roles", "identity");
+
+        builder.Entity<IdentityUser>()
+            .ToTable("Users", "identity");
+
+        builder.Entity<IdentityRoleClaim<string>>()
+            .ToTable("RoleClaims", "identity");
+
+        builder.Entity<IdentityUserClaim<string>>()
+            .ToTable("UserClaims", "identity");
+
+        builder.Entity<IdentityUserLogin<string>>()
+            .ToTable("UserLogins", "identity");
+
+        builder.Entity<IdentityUserRole<string>>()
+            .ToTable("UserRoles", "identity");
+
+        builder.Entity<IdentityUserToken<string>>()
+            .ToTable("UserTokens", "identity");
     }
 }
